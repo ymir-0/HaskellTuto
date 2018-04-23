3c53157d7daa6a63b7284d08047de2506f557b44
ymir <mazieres.pierre@gmail.com>
2018-04-23 09:49:16 +0200

    reverse digits

--- app/01_homework.hs
+++ app/01_homework.hs
@@ -4,8 +4,7 @@ toDigits number
  | number <= 0 = []
  | otherwise = toDigits (div number 10) ++ [mod number 10]
 -- reverse a list
---toDigitsRev :: [Integer] -> [Integer]
---toDigitsRev list
--- | [] = []
--- | element : [] = [element]
--- | element : leftover = toDigitsRev leftover ++ [element]
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev number
+ | number <= 0 = []
+ | otherwise = mod number 10 : toDigitsRev (div number 10)
