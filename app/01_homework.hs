1806b47947c15eeeaeaef27820173e6b10d66241
ymir <mazieres.pierre@gmail.com>
2018-04-23 11:11:05 +0200

    double 2nd from last

--- app/01_homework.hs
+++ app/01_homework.hs
@@ -8,3 +8,14 @@ toDigitsRev :: Integer -> [Integer]
 toDigitsRev number
  | number <= 0 = []
  | otherwise = mod number 10 : toDigitsRev (div number 10)
+-- reverse array
+reverseArray :: [Integer] -> [Integer]
+reverseArray [] = []
+reverseArray (element:[]) = [element]
+reverseArray (element:leftover) = reverseArray(leftover)++[element]
+-- double every 2nd number from right to left
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther [] = []
+doubleEveryOther (element:[]) = element:[]
+doubleEveryOther (element1:element0:[]) = element1:2*element0:[]
+doubleEveryOther (element1:element0:leftover) = element1:2*element0:doubleEveryOther(reverseArray(leftover))
